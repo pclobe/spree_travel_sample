f1e874f44d92e45d253c1828678b13bb6b6123dd
Default <p.cloberre@gmail.com>
2017-03-27 21:37:31 +0200

    Removed some stuff and renamed old deps

--- db/travel_samples/cars/taxons.rb
+++
@@ -1,7 +0,0 @@
-taxons = [
-  { :name => "Car", :taxonomy => "Categories", :parent => 'Categories'}
-]
-
-taxons.each do |taxon_attrs|
-  Spree::TravelSample.create_taxon(taxon_attrs)
-end
--- db/travel_samples/cruises/taxons.rb
+++
@@ -1,7 +0,0 @@
-taxons = [
-  { :name => "Cruise", :taxonomy => "Categories", :parent => 'Categories'}
-]
-
-taxons.each do |taxon_attrs|
-  Spree::TravelSample.create_taxon(taxon_attrs)
-end
